ce7c09d6a0d82b066f92ec0c3b9986b93bdb4b69
Kateryna Zashcholkina <kateryna.zashcholkina@gmail.com>
2026-09-16 20:29:03 +0200

    Configure Entity Framework Core and database connection

---
+++ Student Housing Management System/Data/StudentHousingDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Student_Housing_Management_System.Data
+{
+    public class StudentHousingDbContext : DbContext
+    {
+        public StudentHousingDbContext(DbContextOptions<StudentHousingDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- Student Housing Management System/Program.cs
+++ Student Housing Management System/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Student_Housing_Management_System.Data;
+
 namespace Student_Housing_Management_System
 {
     public class Program
@@ -8,6 +11,10 @@ namespace Student_Housing_Management_System
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<StudentHousingDbContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("StudentHousingDatabase")));
 
             var app = builder.Build();
 
